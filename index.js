2cb90a1e5f17aa66906043ee605cc7c82c081af7
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-09-02 22:06:40 -0400

    Adding changes

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/users', (req,res) => {
+    res.status(200).json([{id:1, name:'Mike'}])
+})
+
+server.post('/api/register', (req,res) => {
+    const newUser = req.body;
+    res.status(201).json(newUser)
+})
+
+server.post('/api/login', (req,res) => {
+    res.status(200).json({ message: "User successfully logged in"})
+})
+
+server.listen(4000, () => {
+    console.log("Listening on port 4000")
+})
