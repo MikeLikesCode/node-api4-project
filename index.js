2e89ea239bb1603be025f035bf63d6ee5c678b9b
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-09-02 22:16:21 -0400

    Adding changes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const server = express();
 
 server.use(express.json())
 server.use(cors())
+
+const PORT = process.env.PORT || 5000
 
 server.get('/', (req, res) => {
     res.json({ message: 'Web 45 is awesome!' })
@@ -26,6 +29,6 @@ server.post('/api/login', (req,res) => {
     res.status(200).json({ message: "User successfully logged in"})
 })
 
-server.listen(4000, () => {
-    console.log("Listening on port 4000")
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
